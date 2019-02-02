b7c06a6933179df56f89085561a0eae520dfb985
David Kellum <dek-oss@gravitext.com>
2019-02-02 08:08:48 -0800

    Add version_check, build.rs just for MSRV
    github cc: rust-lang/rfcs#2495 rust-lang/rfcs#2523

---
+++ build.rs
@@ -0,0 +1,16 @@
+extern crate version_check;
+
+fn main() {
+    match version_check::is_min_version("1.32.0") {
+        Some((true, actual_v)) => {
+            eprintln!("olio MSRV test passed: {} (actual)", actual_v)
+        }
+        Some((false, actual_v)) => {
+            panic!("olio MSRV is 1.32.0 > {} (actual)",
+                   actual_v);
+        }
+        None => {
+            panic!("couldn't query version info from rustc");
+        }
+    }
+}
