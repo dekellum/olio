e4b8f20ff8c86d06892f19437048e2268ed063dc
David Kellum <dek-oss@gravitext.com>
2019-10-02 13:10:55 -0700

    another lock-min annotation

--- lock-minimize.sh
+++ lock-minimize.sh
@@ -23,4 +23,12 @@ cargo -Z minimal-versions generate-lockfile
 # └── tempfile v3.1.0 (*)
 cargo update -p cfg-if --precise 0.1.2
 
+# due to:
+# winapi v0.2.0
+# └── iovec v0.1.0
+#     └── bytes v0.4.6
+#         [dev-dependencies]
+#         └── olio v1.2.0 (/home/david/src/olio)
+# An upgrade to winapi (0.3, in 2017!) was never released:
+# https://github.com/carllerche/iovec/commit/b90b433f58fb8d64ad6c67d8080cf3da1fce3543
 cargo update -p winapi:0.2.0 --precise 0.2.8
